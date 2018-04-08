fe7a050597aafa679078ea7d70c5e1cadbb31961
Sanghou <tkdgh97531@kaist.ac.kr>
2018-04-09 02:46:17 +0900

    ho

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -13,8 +13,46 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
+
+  //read system call number
+  char *ptr = (char *)f->esp; 
+  int sys_num = (int) *ptr;
+
+  switch(sys_num){
+  	/* System call for pintos proj2 */
+  	case SYS_HALT:
+  		break;
+  	case SYS_EXIT:
+  		// exit(0);
+  		break;
+  	case SYS_EXEC:
+  		break;
+  	case SYS_WAIT:
+  		break;
+  	case SYS_CREATE:
+  		break;
+  	case SYS_REMOVE:
+  		break;
+  	case SYS_OPEN:
+  		break;
+  	case SYS_FILESIZE:
+  		break;
+  	case SYS_READ:
+  		break;
+  	case SYS_WRITE:
+  		printf("write");
+  		break;
+  	case SYS_SEEK:
+  		break;
+  	case SYS_TELL:
+  		break;
+  	case SYS_CLOSE:
+  		break;
+  	default:
+  		break;
+  }
   thread_exit ();
 }
