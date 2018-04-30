a3634e4f1238e09bafcdf9a933916689e24f7f65
Sanghou <tkdgh97531@kaist.ac.kr>
2018-04-30 21:44:46 +0900

    change palloc

--- pintos/src/threads/palloc.c
+++ pintos/src/threads/palloc.c
@@ -10,7 +10,9 @@
 #include "threads/loader.h"
 #include "threads/synch.h"
 #include "threads/vaddr.h"
-
+#ifdef VM
+#include "vm/frame.h"
+#endif
 /* Page allocator.  Hands out memory in page-size (or
    page-multiple) chunks.  See malloc.h for an allocator that
    hands out smaller chunks.
@@ -90,6 +92,12 @@ palloc_get_multiple (enum palloc_flags flags, size_t page_cnt)
     {
       if (flags & PAL_ZERO)
         memset (pages, 0, PGSIZE * page_cnt);
+    #ifdef VM 
+      if (flags & PAL_USER) {
+        unsigned page = (int) pages >> 12;
+        allocate_frame_elem(page, page);
+      }
+    #endif
     }
   else 
     {
@@ -135,6 +143,13 @@ palloc_free_multiple (void *pages, size_t page_cnt)
 
 #ifndef NDEBUG
   memset (pages, 0xcc, PGSIZE * page_cnt);
+#endif
+
+#ifdef VM
+  if (pool == &user_pool){
+    unsigned page = (int) pages >> 12;
+    deallocate_frame_elem(page);
+  }
 #endif
 
   ASSERT (bitmap_all (pool->used_map, page_idx, page_cnt));
